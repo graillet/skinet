2b362371322799ff5aebef25b488b095d20f96ea
graillet <luis.graillet@dxc.com>
2022-01-09 22:12:23 -0600

    End of section 11

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { Router, RouterModule, Routes } from '@angular/router';
